d1baf41585c8ed118669708afd1eb93f7ed0b41c
Sami Niemi <sami.niemi@pilvia.com>
2017-09-12 08:38:24 +0300

    initial release

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# cli
+# Pilvia CLI
---
+++ pilvia.js
@@ -0,0 +1,3 @@
+#! /usr/bin/env node
+console.log("Hi! Pilvia CLI is under development, please jump in to https://pilvia.com to get informed when it is ready to rock!")
+
